5cca5d41d668a9d4f569f06bdde3e3a294f26270
Dhanwanthari Ramakrishnan <dan13ram@gmail.com>
2020-07-16 18:32:15 +0530

    empty daemon actions

--- src/actions/daemon.actions.js
+++ src/actions/daemon.actions.js
@@ -3,37 +3,10 @@ import { alertActions } from "./";
 import { daemonConstants } from "../constants";
 
 export const daemonActions = {
-    loadDaemon,
     clean
 };
 
-function loaddaemon(account) {
-    return async (dispatch, getState) => {
-        dispatch(started());
-        let daemon;
-        try {
-            const daemon = await Box.openBox(account, window.ethereum);
-            console.log(daemon);
-            //authenticate users's space for ipfsethmarketplace app
-            //for new user it will create ipfsethmarketplace space
-            //for exissting user it will access his ipfsethmarketplace space
-            space = await daemon.openSpace(daemonConstants.SPACE_NAME);
-            console.log(space);
-            //wait till user's data is synced from network
-            await daemon.syncDone;
-            window.daemon = daemon;
-            await space.syncDone;
-        } catch (e) {
-            console.log(e);
-            dispatch(failure(e.toString()));
-            let error = "Could not load 3Box";
-            dispatch(alertActions.error(error));
-            return;
-        }
-        dispatch(loaded({ daemon, space, loggedIn: true }));
-        dispatch(alertActions.success("Login with 3Box Successful"));
-    };
-}
+const daemon = new SpaceClient({url: `http://0.0.0.0:9998`});
 
 function clean() {
     return dispatch => {
