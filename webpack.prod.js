491541b5ce7e2bd31e2869af97b85a96c32b2c6f
Viraz <virajm72@gmail.com>
2020-08-01 09:54:24 +0530

    Added new abi and address

--- webpack.prod.js
+++ webpack.prod.js
@@ -7,7 +7,7 @@ module.exports = merge(common, {
     externals: {
         config: JSON.stringify({
             networkId: 4,
-            marketAddress: "0xaad639b52aa20aa5147014e9bfb1856b3a429d98",
+            marketAddress: "0xcb3eaafa1a9335d2993c3021b8e089150a0233fd",
             priceAssets: [
                 {
                     address: "0xc3dbf84Abb494ce5199D5d4D815b10EC29529ff8",
